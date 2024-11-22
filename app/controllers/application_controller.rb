4b309f3b3fdc719ba9292df1e8772c60377ca26c
dwlknsn <dwlknsn@users.noreply.github.com>
2024-11-22 11:12:28 +0700

    Add Kittens MVC

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  def record_not_found
+    flash[:error] = "Record not found"
+    redirect_to root_path
+  end
 end
---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,50 @@
+class KittensController < ApplicationController
+  before_action :set_kitten, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show; end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:notice] = "Kitten created successfully"
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @kitten.update(kitten_params)
+      flash[:notice] = "Kitten edited successfully"
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten.destroy
+    flash[:notice] = "Kitten deleted successfully"
+    redirect_to kittens_path
+  end
+
+  private
+
+  def set_kitten
+    @kitten ||= Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
